ea0ec533e7f64b2e64d06e2c8205d85d0932581c
FULLSTACK <jumin0220@naver.com>
2023-04-25 15:48:15 +0900

    스프링-프로젝트-성적조회2

--- src/main/java/jiho/spring4/sungjukv6/controller/SungJukController.java
+++ src/main/java/jiho/spring4/sungjukv6/controller/SungJukController.java
@@ -2,9 +2,11 @@ package jiho.spring4.sungjukv6.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import jiho.spring4.sungjukv6.service.SungJukV6Service;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SungJukController {
@@ -17,8 +19,11 @@ public class SungJukController {
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list() {
+    public String list(Model model) {
         System.out.println(sjsrv.readSungJuk());
+
+       //sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
+        model.addAttribute("sjs",sjsrv.readSungJuk());
 
         return "sungjuklist";
     }
--- src/main/webapp/WEB-INF/views/sungjuklist.jsp
+++ src/main/webapp/WEB-INF/views/sungjuklist.jsp
@@ -1,6 +1,7 @@
 
 
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
   <title>성적리스트</title>
@@ -8,7 +9,19 @@
 <body>
 <table border="1" width="750px" cellpadding="10px" cellspacing="0">
   <tr><th>이름</th><th>국어</th><th>영어</th><th>수학</th></tr>
+
+  <%-- for(SungJuk sj:sjs)--%>
+  <c:forEach items="${sjs}" var="sj">
+    <tr>
+      <td>${sj.name}</td>
+      <td>${sj.kor}</td>
+      <td>${sj.eng}</td>
+      <td>${sj.mat}</td>
+    </tr>
+  </c:forEach>
+
 </table>
 
+<p>${sjs}</p>
 </body>
 </html>
