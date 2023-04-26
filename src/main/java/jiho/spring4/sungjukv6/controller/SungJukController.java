5ae77c511fc9370dc99651d8feae168d04032e81
FULLSTACK <jumin0220@naver.com>
2023-04-26 13:06:59 +0900

    스프링mvc-프로젝트-성적저장2

--- src/main/java/jiho/spring4/sungjukv6/controller/SungJukController.java
+++ src/main/java/jiho/spring4/sungjukv6/controller/SungJukController.java
@@ -1,8 +1,11 @@
 package jiho.spring4.sungjukv6.controller;
 
+import jiho.spring4.sungjukv6.model.SungJukVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import jiho.spring4.sungjukv6.service.SungJukV6Service;
@@ -18,14 +21,31 @@ public class SungJukController {
         this.sjsrv = sjsrv;
     }
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list(Model model) {
-        System.out.println(sjsrv.readSungJuk());
+    //성적 리스트 처리
+    @GetMapping(value = "/list")
+    public ModelAndView list(  ) {
+        ModelAndView mv = new ModelAndView();
 
-       //sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
-        model.addAttribute("sjs",sjsrv.readSungJuk());
+        // sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
+        mv.addObject("sjs", sjsrv.readSungJuk());
+        mv.setViewName("sungjuklist");  // 뷰 이름 지정
 
-        return "sungjuklist";
+        return mv;
+    }
+
+    //성적입력폼표시
+    @GetMapping("/add")
+    public String add(){
+        return "sungjuk";
+    }
+
+    //성적입력처리
+    @PostMapping("/add")
+    public ModelAndView addok(SungJukVO sj){
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("sj",sj);
+        mv.setViewName("sungjukok");
+        return mv;
     }
 
 }
