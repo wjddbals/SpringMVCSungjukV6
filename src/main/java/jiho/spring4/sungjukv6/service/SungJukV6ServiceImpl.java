74e2aac4bc6d070ca22ea862fd8f29569ef20a92
FULLSTACK <jumin0220@naver.com>
2023-04-26 16:58:56 +0900

    스프링mvc-프로젝트-성적삭제

--- src/main/java/jiho/spring4/sungjukv6/service/SungJukV6ServiceImpl.java
+++ src/main/java/jiho/spring4/sungjukv6/service/SungJukV6ServiceImpl.java
@@ -23,7 +23,8 @@ public class SungJukV6ServiceImpl implements SungJukV6Service {
     }
 
     public boolean removeSungJuk(int sjno) {
-    return false;
+        sjdao.deleteSungJuk(sjno);
+        return true;
     }
 
     public boolean modifySungJuk(SungJukVO sj) {
